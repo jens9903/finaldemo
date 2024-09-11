4abd133a3f838fbb04d0611e99eb61eacc230da2
Your Name <your.email@example.com>
2024-09-11 17:20:07 +0530

    change file formate

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
- import './index.js';
+import App from './App.jsx';
+ import './index.jsx';
  
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
